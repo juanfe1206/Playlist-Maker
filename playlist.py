4f5b963bbd65dad2df5bdc6d7558cfdd0d23d541
juanfe1206 <juanfeortizlejarde@gmail.com>
2024-09-29 19:19:03 +0200

    fix issues

--- playlist.py
+++ playlist.py
@@ -37,7 +37,7 @@ def click_button_by_testid(driver, testid):
     element = WebDriverWait(driver, 12).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, f'[data-testid="{testid}"]'))
     )
-    element.click()
+    driver.execute_script("arguments[0].click();", element)
   except selenium.common.exceptions.TimeoutException:
     print("button not found")
     
@@ -58,14 +58,14 @@ def send_text_to_button_by_id(driver, id, text):
     print("")
 
 #complex functions:
-def name_playlist(driver, text_field_id, save):
+def name_playlist(driver, text_field_id, save, playlist_name):
   try:
     element = WebDriverWait(driver, 12).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, f'[data-testid="{text_field_id}"]'))
     )
     element.click()
     element.clear()
-    element.send_keys(mood)
+    element.send_keys(playlist_name)
     click_button_by_testid(driver, save)
   except selenium.common.exceptions.TimeoutException:
     print("button not found")
@@ -78,7 +78,7 @@ def add_song(driver, xpath, song, add):
     element.click()
     element.clear()
     element.send_keys(song)
-    time.sleep(1)
+    time.sleep(1.3)
     click_button_by_testid(driver, add)
   except selenium.common.exceptions.TimeoutException:
     print("button not found")
@@ -92,12 +92,12 @@ def login(driver):
   time.sleep(2)
   click_buttons_by_id(driver, 'onetrust-accept-btn-handler')
  
-def create_playlist(driver, my_playlist_name):
+def create_playlist(driver, playlist_name):
   click_button_by_aria_label(driver, 'Create playlist or folder')
   click_buttons_by_class(driver, 'mWj8N7D_OlsbDgtQx5GW')
   click_buttons_by_class(driver, 'wCkmVGEQh3je1hrbsFBY')
   time.sleep(0.6)
-  name_playlist(driver, 'playlist-edit-details-name-input', save=my_playlist_name)
+  name_playlist(driver, 'playlist-edit-details-name-input', save='playlist-edit-details-save-button', playlist_name=playlist_name)
   
 def create_list_of_songs(mood: str, artists: str):
   dotenv.load_dotenv()
@@ -113,9 +113,9 @@ def create_list_of_songs(mood: str, artists: str):
            "content": """
            You are a music dj that knows a lot about a wide variety of genres and up and coming artists. 
            Your task will be to receive a mood or prompt from the user who will tell you what kind of music they want to hear and optionally a few artists as reference.
-           Then, with this information your task is to only return a list of songs of size 15 and no additional text as follow:
-           'song - Artist, Song - Artist, song - Artist'
-           If no artists are given as reference, it is up to you to choose artists that fit the mood, it is specially good in this case to use popular artists.
+           Then, with this information your task is to only return a list of songs of size 15 and no additional text as follow. This is the most important task so make sure to do it correctly.
+           Example: 'All too well (Taylor's version) - Taylor Swift, Song - Artist, song - Artist'
+           If no artists are given as reference, it is up to you to choose the songs and artists that fit the mood, it is specially good in this case to use popular artists and songs.
            Adittionaly, since the list will be split using commas, make sure to avoid using them inside the names of the songs
            """},
           {
@@ -136,7 +136,7 @@ def create_list_of_songs(mood: str, artists: str):
            You are a music dj that knows a lot about a wide variety of genres and up and coming artists. 
            Your task will be to receive a list of songs from various artists and from there, you will have to create a short and creative name for a playlist. 
            The more creative and in sync with the music you are given the better, you can use metaphors and other figures of speech to make the coolest name possible.
-           Finally, when you return the name, make sure to only return the name you created and nothing else.
+           Finally, when you return the name, make sure to only return the name you created and nothing else. If you can roast the user with the title, do so.
            example: Summery Vibes
            """},
           {
@@ -157,7 +157,8 @@ def create_list_of_songs(mood: str, artists: str):
 dotenv.load_dotenv()
 
 #Call openai api to create the 
-list_of_songs, my_playlist_name = create_list_of_songs('happy pop songs', artists='Taylor Swift, Billie Eilish')
+list_of_songs, my_playlist_name = create_list_of_songs('Depressed', artists='Taylor Swift (Taylors Version)')
+print(list_of_songs)
 
 #start selenium
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -165,9 +166,13 @@ driver.get("https://accounts.spotify.com/en/login")
 
 login(driver)
 time.sleep(1)
-create_playlist(driver, my_playlist_name)
+create_playlist(driver, playlist_name=my_playlist_name)
 time.sleep(0.5)
 for song in list_of_songs:
   add_song(driver, '/html/body/div[5]/div/div[2]/div[4]/div[1]/div[2]/div[2]/div/main/section/div[2]/div[3]/div/section/div/div/input', song, 'add-to-playlist-button')
-
+  time.sleep(0.3)
+  
+time.sleep(1)
+#Start playing music
+click_button_by_testid(driver, 'play-button')
 input()
